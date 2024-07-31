e4b40fd3edcf6570cbac21d77d2be2c1355a8e5a
cm8908 <jungminseob2@gmail.com>
2024-07-31 09:59:19 +0000

    [add] scripts for eval(captioncad, openecad), train(openecad)

---
+++ scripts/eval/openecad.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+gpu_list="${CUDA_VISIBLE_DEVICES:-0}"
+IFS=',' read -ra GPULIST <<< "$gpu_list"
+
+CHUNKS=${#GPULIST[@]}
+
+MODEL_PATH="/home/jung/TinyLLaVA_Factory/outputs/OpenECAD-Gemma-SigLip-2.4B-lora-split"
+MODEL_NAME="OpenECAD-Gemma-SigLip-2.4B-lora-split"
+EVAL_IMG_DIR="/home/jung/TinyLLaVA_Factory/dataset/openecad/images_3d_test"
+EVAL_TEXT_PATH="/home/jung/TinyLLaVA_Factory/dataset/text_files/data_3d_lite_test.jsonl"
+MAX_TOKENS=3072
+
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m tinyllava.eval.model_vqa_loader \
+        --model-path $MODEL_PATH \
+        --question-file $EVAL_TEXT_PATH \
+        --image-folder $EVAL_IMG_DIR \
+        --answers-file $MODEL_PATH/generated/${CHUNKS}_${IDX}.jsonl \
+        --num-chunks $CHUNKS \
+        --chunk-idx $IDX \
+        --temperature 0 \
+        --max_new_tokens $MAX_TOKENS \
+        --conv-mode gemma &
+done
+
+wait
+
+output_file=$MODEL_PATH/generated/merge.jsonl
+
+# Clear out the output file if it exists.
+> "$output_file"
+
+# Loop through the indices and concatenate each file.
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    cat $MODEL_PATH/generated/${CHUNKS}_${IDX}.jsonl >> "$output_file"
+done
